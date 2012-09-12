bc3b1aecd19d1bc421bfe65a05f45fafab4030a7
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2012-09-11 21:36:50 -0400

    comments added

--- touchmove.js
+++ touchmove.js
@@ -1,8 +1,15 @@
+// Object Reference
 var obj = document.getElementById('id');
+
+// Object Event Listener
 obj.addEventListener('touchmove', function(event) {
+
   // If there's exactly one finger inside this element
   if (event.targetTouches.length == 1) {
+	
+  	// The Touches
     var touch = event.targetTouches[0];
+
     // Place element where the finger is
     obj.style.left = touch.pageX + 'px';
     obj.style.top = touch.pageY + 'px';
