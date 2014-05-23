0f9c1e1ef4b5f3a12a75916bd8fe76085a83c8ec
James Chochlinski <jchochlinski@xpzen.com>
2014-05-22 21:25:23 -0400

    added eclipse projects

---
+++ Week1_Practice/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World");
+	}
+
+}
