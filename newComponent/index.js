eb8d350f86c8f76f8f3c89c95577c638ae22b8de
John Martin <jrmartin182@gmail.com>
2021-02-15 10:24:22 -0700

    BUILD: added component folder and index.js
    BUILD: added p tag with text
    
    BUILD: added another p tag with text to create more commit messages

---
+++ newComponent/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const newComponent = () => {
+    return (
+        <div>
+            <p>Testing out git features such as fetch, adding an upstream, creating and switching to a new branch,
+                 squashing and whatever else is in the tutorial Im following.</p>
+            <p>Thanks for the practice space!</p>
+        </div>
+    )
+}
+
+export default newComponent
